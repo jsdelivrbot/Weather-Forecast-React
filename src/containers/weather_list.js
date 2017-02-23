abdc9758ace61eacb688d7133ce6604238ae9a29
armaango <armaan.goyal@gmail.com>
2017-02-23 00:40:42 -0800

    Added Google maps

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,21 +1,44 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-
+import Chart from '../components/chart';
+import GoogleMap from '../components/googlemap';
 class WeatherList extends Component {
+
+    renderWeather(cityData) {
+        const name = cityData.city.name;
+        const temps = cityData.list.map(weather => weather.main.temp);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+        const {lon,lat} = cityData.city.coord;
+
+        return (
+            <tr key={name}>
+                <td><GoogleMap lon={lon} lat={lat}/></td>
+                <td>
+                    <Chart data = {temps} color = "red" units="K"/>
+                </td>
+                <td>
+                    <Chart data = {pressures} color = "blue" units="hPa"/>
+                </td>
+                <td>
+                    <Chart data = {humidities} color = "orange" units="%"/>
+                </td>
+            </tr>
+        ); 
+    }
     render () {
         return (
             <table className="table table-hover">
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature(K)</th>
+                        <th>Pressure(hPA)</th>
+                        <th>Humidity(%)</th>
                     </tr>
                 </thead>
                 <tbody>
-
+                    {this.props.weather.map(this.renderWeather)}
                 </tbody>
             </table>
         );
